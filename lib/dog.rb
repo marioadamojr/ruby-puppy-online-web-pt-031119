c1916741d20b70e81ae064d5e33b89ba74d17866
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-04 15:46:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
+  attr_accessor :name
   @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+    
+
 
   def self.clear_all
     @@all = []
   end
 
-
+  def self.all
+    
+  end
 end
